8d5c7a117f0fe86db4240ff8a2a4ec3f0bbed744
maryam najafi <maryam_najafi73@yahoo.com>
2022-04-22 13:41:16 +0430

    [add] add Indexer module to trainer

--- src/trainer.py
+++ src/trainer.py
@@ -16,11 +16,12 @@ from transformers import T5Tokenizer, T5EncoderModel
 
 from configuration import BaseConfig
 from data_loader import read_csv
+from indexer import Indexer
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    # create config instance
+    # -------------------------------- Create config instance----------------------------------
     CONFIG_CLASS = BaseConfig()
     ARGS = CONFIG_CLASS.get_config()
     MT5_TOKENIZER = T5Tokenizer.from_pretrained(ARGS.language_model_tokenizer_path)
@@ -29,7 +30,7 @@ if __name__ == "__main__":
     # create CSVLogger instance
     LOGGER = CSVLogger(save_dir=ARGS.saved_model_path, name=ARGS.model_name)
 
-    # load raw data
+    # ----------------------------------- Load raw data------------------------------------------
     TRAIN_DATA = read_csv(path=os.path.join(ARGS.processed_data_dir, ARGS.train_file),
                           columns=ARGS.data_headers,
                           names=ARGS.customized_headers).dropna()
@@ -44,4 +45,15 @@ if __name__ == "__main__":
                           columns=ARGS.data_headers,
                           names=ARGS.customized_headers).dropna()
     logging.info('valid set contain %s sample ...', len(VALID_DATA))
+    # ------------------------------------- Indexer --------------------------------------------
+    TARGET_INDEXER = Indexer(vocabs=list(TRAIN_DATA.targets))
+    TARGET_INDEXER.build_vocab2idx()
 
+    TRAIN_TARGETS_CONVENTIONAL = [[target] for target in list(TRAIN_DATA.targets)]
+    TRAIN_INDEXED_TARGET = TARGET_INDEXER.convert_samples_to_indexes(TRAIN_TARGETS_CONVENTIONAL)
+
+    TEST_TARGETS_CONVENTIONAL = [[target] for target in list(TEST_DATA.targets)]
+    TEST_INDEXED_TARGET = TARGET_INDEXER.convert_samples_to_indexes(TEST_TARGETS_CONVENTIONAL)
+
+    VALID_TARGETS_CONVENTIONAL = [[target] for target in list(VALID_DATA.targets)]
+    VALID_INDEXED_TARGET = TARGET_INDEXER.convert_samples_to_indexes(VALID_TARGETS_CONVENTIONAL)
