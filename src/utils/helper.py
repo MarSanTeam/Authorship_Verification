e7a7c7bd1bdeed0fd7004fd19fb44f6279ad5c86
ehsan.tavan <tavan.ehsan@gmail.com>
2022-04-28 10:45:10 +0430

    [refactor] use space before add character

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -16,6 +16,6 @@ def extract_punctuation(texts: List[str]) -> List[str]:
     print()
     for text in texts:
         text = re.sub(pattern, "", text)
-        punc = "".join(ch for ch in text if ch in exclude)
+        punc = " ".join(ch for ch in text if ch in exclude)
         punctuations.append(punc)
     return punctuations
