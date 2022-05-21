a073ace63dc325a5c1668432388886dab132f0d7
Ehsan-Tavan <tavan.ehsan@gmail.com>
2022-05-22 01:23:49 +0430

    [add] add load data and model prediction

--- src/inferencer.py
+++ src/inferencer.py
@@ -1,19 +1,65 @@
-import os
+import logging
+from torch.utils.data import DataLoader
+
 from configuration import BaseConfig
 from transformers import T5Tokenizer
 from models.t5_encoder_pos import Classifier
-from utils import prepare_test_data
+from utils import prepare_test_data, extract_punctuation_emoji, extract_information, \
+    extract_pos
+from dataset import InferenceDataset
+
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
     # ----------------------------------- instantiation -------------------------------
     CONFIG_CLASS = BaseConfig()
     ARGS = CONFIG_CLASS.get_config()
 
-    MODEL_PATH = ""
+    MODEL_PATH = "../assets/saved_models/version_115/" \
+                 "checkpoints/QTag-epoch=55-val_acc=0.92.ckpt"
+    DATA_PATH = "../data/Raw/pairs.jsonl"
 
-    first_authors_texts, second_authors_texts = prepare_test_data(path="")
+    FIRST_AUTHORS_TEXTS, SECOND_AUTHORS_TEXTS = prepare_test_data(path=DATA_PATH)
+    FIRST_AUTHORS_TEXTS = FIRST_AUTHORS_TEXTS[:10]
+    SECOND_AUTHORS_TEXTS = SECOND_AUTHORS_TEXTS[:10]
+    logging.info("Data was loaded")
+    logging.info("We have %d data", len(FIRST_AUTHORS_TEXTS))
 
     T5_TOKENIZER = T5Tokenizer.from_pretrained(ARGS.language_model_tokenizer_path)
+    logging.info("Tokenizer was loaded")
     MODEL = Classifier.load_from_checkpoint(MODEL_PATH)
+    logging.info("Model was loaded")
 
+    FIRST_TEXT_PUNCTUATIONS = extract_punctuation_emoji(FIRST_AUTHORS_TEXTS)
+    SECOND_TEXT_PUNCTUATIONS = extract_punctuation_emoji(SECOND_AUTHORS_TEXTS)
+    logging.info("Punctuations and emojis are extracted")
 
+    FIRST_TEXT_INFORMATION = extract_information(FIRST_AUTHORS_TEXTS)
+    SECOND_TEXT_INFORMATION = extract_information(SECOND_AUTHORS_TEXTS)
+    logging.info("Information are extracted")
+
+    FIRST_TEXT_POS = extract_pos(FIRST_AUTHORS_TEXTS)
+    SECOND_TEXT_POS = extract_pos(SECOND_AUTHORS_TEXTS)
+    logging.info("Pos are extracted")
+
+    DATA = {"first_text": FIRST_AUTHORS_TEXTS,
+            "second_text": SECOND_AUTHORS_TEXTS,
+            "first_punctuations": FIRST_TEXT_PUNCTUATIONS,
+            "second_punctuations": SECOND_TEXT_PUNCTUATIONS,
+            "first_information": FIRST_TEXT_INFORMATION,
+            "second_information": SECOND_TEXT_INFORMATION,
+            "first_pos": FIRST_TEXT_POS,
+            "second_pos": SECOND_TEXT_POS}
+
+    DATASET = InferenceDataset(data=DATA, tokenizer=T5_TOKENIZER,
+                               max_len=ARGS.max_len)
+
+    DATALOADER = DataLoader(DATASET, batch_size=1,
+                            shuffle=False, num_workers=4)
+
+    for i_batch, sample_batched in enumerate(DATALOADER):
+        sample_batched["input_ids"] = sample_batched["input_ids"].to("cuda:0")
+        sample_batched["punctuation"] = sample_batched["punctuation"].to("cuda:0")
+        sample_batched["information"] = sample_batched["information"].to("cuda:0")
+        sample_batched["pos"] = sample_batched["pos"].to("cuda:0")
+        OUTPUT = MODEL(sample_batched)
