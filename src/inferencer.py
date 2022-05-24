fcf0b9cc53e5fa4b2d160fbc95a6729c57829caf
ehsan.tavan <ehsan.tavan@partsoftware.com>
2022-05-24 11:59:40 +0430

    [add] add handle_pos_tags

--- src/inferencer.py
+++ src/inferencer.py
@@ -3,10 +3,11 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 from configuration import BaseConfig
+from data_loader import read_json
 from transformers import T5Tokenizer
 from models.t5_encoder_pos import Classifier
 from utils import prepare_test_data, extract_punctuation_emoji, extract_information, \
-    extract_pos
+    extract_pos, handle_pos_tags
 from dataset import InferenceDataset
 
 logging.basicConfig(level=logging.DEBUG)
@@ -23,6 +24,8 @@ if __name__ == "__main__":
     FIRST_AUTHORS_TEXTS, SECOND_AUTHORS_TEXTS = prepare_test_data(path=DATA_PATH)
     FIRST_AUTHORS_TEXTS = FIRST_AUTHORS_TEXTS[:10]
     SECOND_AUTHORS_TEXTS = SECOND_AUTHORS_TEXTS[:10]
+
+    POS_VOCAB2IDX = read_json(path="../assets/token_vocab2idx.json")
     logging.info("Data was loaded")
     logging.info("We have %d data", len(FIRST_AUTHORS_TEXTS))
 
@@ -41,6 +44,8 @@ if __name__ == "__main__":
 
     FIRST_TEXT_POS = extract_pos(FIRST_AUTHORS_TEXTS)
     SECOND_TEXT_POS = extract_pos(SECOND_AUTHORS_TEXTS)
+    FIRST_TEXT_POS = handle_pos_tags(FIRST_TEXT_POS, POS_VOCAB2IDX)
+    SECOND_TEXT_POS = handle_pos_tags(SECOND_TEXT_POS, POS_VOCAB2IDX)
     logging.info("Pos are extracted")
 
     DATA = {"first_text": FIRST_AUTHORS_TEXTS,
@@ -55,9 +60,10 @@ if __name__ == "__main__":
     DATASET = InferenceDataset(data=DATA, tokenizer=T5_TOKENIZER,
                                max_len=ARGS.max_len)
 
-    DATALOADER = DataLoader(DATASET, batch_size=1,
+    DATALOADER = DataLoader(DATASET, batch_size=4,
                             shuffle=False, num_workers=4)
 
+    PREDICTIONS = []
     for i_batch, sample_batched in enumerate(DATALOADER):
         sample_batched["input_ids"] = sample_batched["input_ids"].to("cuda:0")
         sample_batched["punctuation"] = sample_batched["punctuation"].to("cuda:0")
@@ -65,6 +71,6 @@ if __name__ == "__main__":
         sample_batched["pos"] = sample_batched["pos"].to("cuda:0")
         OUTPUT = MODEL(sample_batched)
         OUTPUT = np.argmax(OUTPUT.cpu().detach().numpy(), axis=1)
-        # OUTPUT = OUTPUT
 
-        print(OUTPUT)
+        PREDICTIONS.extend(OUTPUT)
+    print(PREDICTIONS)
